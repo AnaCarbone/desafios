3dcf4addd049eceec357fcebe7a9a6ca404f6929
Ana Paula Carbone <anabcarbone@gmail.com>
2019-06-08 12:00:32 -0300

    Update cheaptrips.cpp

--- cheaptrips.cpp
+++ cheaptrips.cpp
@@ -2,27 +2,28 @@
 
 using namespace std;
 
-long long dp[11234];
+long long dp[10001];
 long long discount[] = {1, 2, 4, 4, 4, 4};
+vector<long long> duration(10001), cost(10001);
 
-long long getMinCost(vector<long long> &duration, vector<long long> &cost, int id) {
-  if (id >= int(duration.size())) return 0;
+long long getMinCost(int index) {
+  if (index >= int(duration.size())) return 0;
 
-  if (dp[id] != -1) return dp[id];
+  if (dp[index] != -1) return dp[index];
 
   long long minCost = LLONG_MAX;
   long long partialCost = 0;
   long long partialDuration = 0;
 
   for (int i = 0; partialDuration < 120 && i < 6 &&
-                  (id + i) < int(duration.size())
+                  (index + i) < int(duration.size())
                 ; i++) {
-    partialCost += cost[id + i]*100/discount[i];
-    minCost = min(minCost, partialCost + getMinCost(duration, cost, id + i + 1));
+    partialCost += cost[index + i]*100/discount[i];
+    minCost = min(minCost, partialCost + getMinCost(index + i + 1));
 
-    partialDuration += duration[id + i];
+    partialDuration += duration[index + i];
   }
-  dp[id] = minCost;
+  dp[index] = minCost;
   return minCost;
 }
 
@@ -30,13 +31,12 @@ int main() {
 
   int n;
   cin >> n;
-  vector<long long> duration(n), cost(n);
   for (int i = 0; i < n; i++) {
     cin >> duration[i] >> cost[i];
   }
 
   memset(dp, -1, sizeof dp);
-  long long minCost = getMinCost(duration, cost, 0);
+  long long minCost = getMinCost(0);
   double resp = (double) minCost/100;
   cout << std::fixed;
   cout << std::setprecision(2);
